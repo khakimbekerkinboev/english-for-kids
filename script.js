9c4eaf3837403e5b25f3a2cb1fa38080bd2c8b78
khakimbekerkinboev <erkinboevkhakimbek@gmail.com>
2022-06-30 22:23:38 +0500

    feat: add main and category pages for the train mode

--- script.js
+++ script.js
@@ -16,6 +16,7 @@ for (let i = 0; i < cards[0].length; i++) {
   sidebarMenu.innerHTML += `<li class="sidebar-category">${cards[0][i]}</li>`
 }
 sidebarMenu.innerHTML += `<li>Statistics</li>`
+const sidebarCategories = document.querySelectorAll('.sidebar-category')
 
 //open
 openSidebarBtn.addEventListener('click', () => {
@@ -49,4 +50,119 @@ sidebarBackground.addEventListener('click', () => {
 
 function enableScrolling() {
   window.onscroll = function () {}
+}
+
+////////////////////////
+//Pages
+////////////////////////
+
+const pageTitle = document.querySelector('.page-title')
+const pageCenter = document.querySelector('.page-center')
+
+//
+//main page
+//
+
+//when the page is loaded
+window.addEventListener('DOMContentLoaded', (event) => {
+  fillMainPage()
+  openCategoryPage()
+})
+
+//the Main page is clicked in the sidebar
+sidebarMenu.children[0].addEventListener('click', () => {
+  fillMainPage()
+  closeSidebar()
+  openCategoryPage()
+})
+
+function fillMainPage() {
+  pageTitle.innerHTML = 'Categories:'
+  pageCenter.innerHTML = ''
+  for (let i = 0; i < cards[0].length; i++) {
+    pageCenter.innerHTML += `
+        <!-- single card -->
+        <div class="category">
+          <img src="./assets/${cards[i + 1][0].image}" alt="" />
+          <h3>${cards[0][i]}</h3>
+          <p>${cards[i + 1].length} cards</p>
+        </div>
+        <!-- end of single card -->`
+  }
+}
+
+//
+//category page
+//
+
+//when the category in the sidebar is clicked
+Array.from(sidebarCategories).forEach((e) => {
+  e.addEventListener('click', () => {
+    fillCategoryPage(e.innerHTML)
+    closeSidebar()
+  })
+})
+
+//when the category card in the main page is clicked
+function openCategoryPage() {
+  const mainPageCategories = document.querySelectorAll('.category')
+  mainPageCategories.forEach((card) => {
+    card.addEventListener('click', (e) => {
+      const clickedCategoryName = e.currentTarget.children[1].innerHTML
+      fillCategoryPage(clickedCategoryName)
+    })
+  })
+}
+
+//randomize indexes of any array by length
+function arrayRandom(length) {
+  let arr = []
+  while (arr.length < length) {
+    const newNumber = Math.floor(Math.random() * length)
+    if (!arr.includes(newNumber)) {
+      arr.push(newNumber)
+    }
+  }
+  return arr
+}
+
+function fillCategoryPage(categoryName) {
+  //change title
+  pageTitle.innerHTML = categoryName
+  //empty the section
+  pageCenter.innerHTML = ''
+  //index
+  const index = cards[0].indexOf(categoryName)
+  //randomize
+  const randomCards = arrayRandom(cards[index + 1].length)
+
+  for (let i = 0; i < cards[index + 1].length; i++) {
+    pageCenter.innerHTML += `
+    <!-- single card -->
+        <div class="single-card">
+          <div class="single-card-inner">
+            <div class="single-card-front">
+              <img src="./assets/${
+                cards[index + 1][randomCards[i]].image
+              }" alt="">
+              <h3>${cards[index + 1][randomCards[i]].word}</h3>
+              <i class="fa-solid fa-language"></i>
+            </div>
+            <div class="hidden-card">
+              <img src="./assets/${
+                cards[index + 1][randomCards[i]].image
+              }" alt="">
+              <h3>${cards[index + 1][randomCards[i]].word}</h3>
+            </div>
+            <div class="single-card-back">
+              <img src="./assets/${
+                cards[index + 1][randomCards[i]].image
+              }" alt="">
+              <h3>${cards[index + 1][randomCards[i]].translation}</h3>
+            </div>
+          </div>
+        </div>
+        <!-- end of single card -->
+    `
+  }
 }
